52e0f915518615c99ee2d04ca4fb7067817e6efe
Arnaud Barisain-Monrose <abarisain@gmail.com>
2024-04-11 15:14:26 +0200

    chore: remove unused code

--- Sources/Private/Consts.h
+++ Sources/Private/Consts.h
@@ -14,13 +14,4 @@
 // Rich notification
 #define BA_ATTACHMENT_IDENTIFIER @"batch_rich_attachment"
 
-// Display receipts
-#define BA_RECEIPT_CACHE_DIRECTORY @"com.batch.displayreceipts"
-#define BA_RECEIPT_CACHE_FILE_FORMAT @"%@.bin"
-#define BA_RECEIPT_MAX_CACHE_FILE 5
-#define BA_RECEIPT_MAX_CACHE_AGE 2592000.0 // 30 days in seconds
-#define BA_RECEIPT_HEADER_EXT_VERSION @"x-batch-ext-version"
-#define BA_RECEIPT_HEADER_SCHEMA_VERSION @"x-batch-protocol-version"
-#define BA_RECEIPT_SCHEMA_VERSION @"1.0.0"
-
 #endif /* Consts_h */
